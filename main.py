3347e5837225ab03a5c2c085efa26507fdb54388
Sergei Samokhvalov <kolobok205@gmail.com>
2020-11-09 00:52:19 +0300

    dialog reload or skip exist torrent

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 
 import base64
 import configparser
+import json
 import logging
 import os.path
+import re
 import signal
 import sys
 from functools import wraps
+from hashlib import sha256
 
-import telegram
 from deluge_client import DelugeRPCClient
-from telegram.ext import MessageHandler, Filters, Updater
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
+from telegram.ext import CallbackQueryHandler, CallbackContext, MessageHandler, Filters, Updater
 
 from schedule_thread import ScheduleThread
 from user_service import UserService
@@ -47,52 +50,172 @@ def restricted(func):
     return wrapped
 
 
-def get_deluge_torrent_name_by_id(deluge_torrent_id):
-    status = deluge_client.core.get_torrent_status(deluge_torrent_id, ['name'])
-    return status['name']
-
-
 @restricted
-def handle_message(update, context):
+def handle_message(update: Update, context: CallbackContext) -> None:
     message = update.message.text
+    chat_id: int = update.effective_chat.id
+    user_id: int = update.effective_user.id
     if isinstance(message, str) and message.startswith('magnet'):
         magnet_uri = message
         try:
-            deluge_torrent_id = deluge_client.core.add_torrent_magnet(magnet_uri, {})
-            torrent_name = get_deluge_torrent_name_by_id(deluge_torrent_id)
-            user_service.create_torrent(update.effective_user.id, deluge_torrent_id)
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Downloading `{0}`".format(torrent_name),
-                                     parse_mode=telegram.ParseMode.MARKDOWN)
+            torrent_name, deluge_torrent_id = start_download_torrent_by_magnet(magnet_uri, user_id)
+            context.bot.send_message(chat_id=chat_id, text="Downloading `{0}`".format(torrent_name),
+                                     parse_mode=ParseMode.MARKDOWN)
         except Exception as e:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Error add torrent: {}".format(str(e)))
+            torrent_id_matcher = torrent_id_matcher_from_exception(e)
+            if type(e) and type(e).__name__ == 'AddTorrentError' and torrent_id_matcher:
+                already_exist_torrent_id = torrent_id_matcher.group(1)
+                cache_key = cache_key_magnet_value(already_exist_torrent_id, user_id)
+                reply_markup = build_reply_markup(already_exist_torrent_id, cache_key)
+                context.bot.send_message(chat_id=chat_id,
+                                         text='Torrent already downloaded, what to do?', reply_markup=reply_markup)
+            else:
+                context.bot.send_message(chat_id=chat_id, text="Error add torrent: {}".format(str(e)))
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id,
+        context.bot.send_message(chat_id=chat_id,
                                  text="Link is not magnet".format(message),
-                                 parse_mode=telegram.ParseMode.MARKDOWN)
+                                 parse_mode=ParseMode.MARKDOWN)
+
+
+@restricted
+def handle_button_callback(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    try:
+        cache_value = user_service.get_cache(query.data)
+        user_id: int = query.from_user.id
+        if not cache_value:
+            raise ValueError(f'cache is not found by key {query.data} for user {user_id} '
+                             f'({query.from_user.first_name})')
+        callback_data = json.loads(cache_value[1])
+        if is_already_exist_callback(callback_data):
+            torrent_id = callback_data['torrent_id']
+            if callback_data['action'] == 'reload':
+                logging.debug('callback_action reload, torrent_id {}'.format(torrent_id))
+                cache_value = user_service.get_cache(callback_data['cache_key'])
+                cache_key = cache_value[0]
+                cache_value = cache_value[1]
+                if not cache_value:
+                    raise ValueError(f'no value for cache')
+
+                if '_magnet_value' in cache_key and cache_value.startswith('magnet'):
+                    deluge_client.core.remove_torrent(torrent_id, False)
+                    torrent_name, deluge_torrent_id = start_download_torrent_by_magnet(cache_value, user_id,
+                                                                                       override_on_exist=True)
+                    query.edit_message_text(f'Downloading `{torrent_name}`', parse_mode=ParseMode.MARKDOWN)
+                elif '_file_value' in cache_key and len(cache_value) > 1:
+                    torrent_name = get_deluge_torrent_name_by_id(torrent_id)
+                    deluge_client.core.remove_torrent(torrent_id, False)
+                    torrent_name, deluge_torrent_id = start_download_torrent_by_file(cache_value,
+                                                                                     f'{torrent_name}.torrent', user_id,
+                                                                                     override_on_exist=True)
+                    query.edit_message_text(f'Downloading `{torrent_name}`', parse_mode=ParseMode.MARKDOWN)
+            if callback_data['action'] == 'skip':
+                logging.debug('callback_action skip, torrent_id {}'.format(torrent_id))
+                query.edit_message_text(f'Torrent `{get_deluge_torrent_name_by_id(torrent_id)}` already exist. '
+                                        f'Skipping download.', parse_mode=ParseMode.MARKDOWN)
+
+    except Exception as e:
+        query.edit_message_text("Sorry, I'm broke. Try to download later.", parse_mode=ParseMode.MARKDOWN)
+        logging.error('error on process callback_data {}, error: {}'.format(query.data, str(e)))
 
 
 @restricted
 def handle_file(update, context):
     file_name = update.message.document.file_name
     root, ext = os.path.splitext(file_name)
+    chat_id: int = update.effective_chat.id
+    user_id: int = update.effective_user.id
     if ext == '.torrent':
+        file_id = update.message.document.file_id
+        file = context.bot.get_file(file_id)
+        base64_file_str = base64.b64encode(file.download_as_bytearray()).decode("utf-8")
         try:
-            file_id = update.message.document.file_id
-            file = context.bot.get_file(file_id)
-            base64_file = base64.b64encode(file.download_as_bytearray())
-            deluge_torrent_id = deluge_client.core.add_torrent_file(file_name, base64_file.decode("utf-8"), {})
-            torrent_name = get_deluge_torrent_name_by_id(deluge_torrent_id)
-            user_service.create_torrent(update.effective_user.id, deluge_torrent_id)
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Downloading `{0}`".format(torrent_name),
-                                     parse_mode=telegram.ParseMode.MARKDOWN)
+            torrent_name, deluge_torrent_id = start_download_torrent_by_file(base64_file_str, file_name, user_id)
+            context.bot.send_message(chat_id=chat_id, text="Downloading `{0}`".format(torrent_name),
+                                     parse_mode=ParseMode.MARKDOWN)
 
         except Exception as e:
-            context.bot.send_message(chat_id=get_deluge_torrent_name_by_id,
-                                     text="Error add torrent file: {}".format(str(e)))
+            torrent_id_matcher = torrent_id_matcher_from_exception(e)
+            if type(e) and type(e).__name__ == 'AddTorrentError' and torrent_id_matcher:
+                already_exist_torrent_id = torrent_id_matcher.group(1)
+                cache_key = cache_key_file_value(already_exist_torrent_id, user_id)
+                user_service.create_cache(cache_key, base64_file_str, override_on_exist=True)
+                reply_markup = build_reply_markup(already_exist_torrent_id, cache_key)
+                context.bot.send_message(chat_id=chat_id,
+                                         text='Torrent already downloaded, what to do?', reply_markup=reply_markup)
+            else:
+                context.bot.send_message(chat_id=get_deluge_torrent_name_by_id,
+                                         text="Error add torrent file: {}".format(str(e)))
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id,
+        context.bot.send_message(chat_id=chat_id,
                                  text="File `{}` is not a `.torrent`".format(file_name),
-                                 parse_mode=telegram.ParseMode.MARKDOWN)
+                                 parse_mode=ParseMode.MARKDOWN)
+
+
+def torrent_id_matcher_from_exception(e):
+    return re.search('^Torrent already in session \\((.+?)\\)\\.$', str(e), re.MULTILINE)
+
+
+def get_deluge_torrent_name_by_id(deluge_torrent_id):
+    status = deluge_client.core.get_torrent_status(deluge_torrent_id, ['name'])
+    return status['name']
+
+
+def sha256_str(skip_callback_data: str) -> str:
+    return sha256(skip_callback_data.encode()).hexdigest()
+
+
+def start_download_torrent_by_magnet(magnet_uri: str, user_id: int, override_on_exist=False) -> (str, str):
+    deluge_torrent_id = deluge_client.core.add_torrent_magnet(magnet_uri, {})
+    torrent_name = get_deluge_torrent_name_by_id(deluge_torrent_id)
+    user_service.create_torrent(user_id, deluge_torrent_id, override_on_exist=override_on_exist)
+    return torrent_name, deluge_torrent_id
+
+
+def is_already_exist_callback(callback_data: dict) -> bool:
+    return callback_data['callback_data_type'] == 'already_exist_torrent'
+
+
+def cache_key_magnet_value(torrent_id: str, user_id: int) -> str:
+    return f'{torrent_id}_{user_id}_magnet_value'
+
+
+def cache_key_file_value(torrent_id: str, user_id: int) -> str:
+    return f'{torrent_id}_{user_id}_file_value'
+
+
+def start_download_torrent_by_file(base64_file_str: str, file_name, user_id: int, override_on_exist=False) -> (
+        str, str):
+    deluge_torrent_id = deluge_client.core.add_torrent_file(file_name, base64_file_str, {})
+    torrent_name = get_deluge_torrent_name_by_id(deluge_torrent_id)
+    user_service.create_torrent(user_id, deluge_torrent_id, override_on_exist=override_on_exist)
+    return torrent_name, deluge_torrent_id
+
+
+def build_reply_markup(already_exist_torrent_id, cache_key):
+    # cache_key is need in case of big magnet url or big base64 of file
+    skip_callback_data = json.dumps({'callback_data_type': 'already_exist_torrent', 'action': 'skip',
+                                     'torrent_id': already_exist_torrent_id, 'cache_key': cache_key})
+    reload_callback_data = json.dumps({'callback_data_type': 'already_exist_torrent', 'action': 'reload',
+                                       'torrent_id': already_exist_torrent_id, 'cache_key': cache_key})
+    # 64 bytes string, this is limit of callback_data
+    skip_callback_data_hash = sha256_str(skip_callback_data)
+    reload_callback_data_hash = sha256_str(reload_callback_data)
+    user_service.create_cache(skip_callback_data_hash, skip_callback_data, override_on_exist=True)
+    user_service.create_cache(reload_callback_data_hash, reload_callback_data, override_on_exist=True)
+    keyboard = [
+        [
+            InlineKeyboardButton("Skip, do nothing", callback_data=skip_callback_data_hash),
+            InlineKeyboardButton("Reload torrent", callback_data=reload_callback_data_hash)
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    return reply_markup
 
 
 def error_callback(update, contex):
@@ -110,11 +233,10 @@ if config.has_section('socks5'):
         }
 
 tg_updater = Updater(config.get('telegram', 'token'), use_context=True, request_kwargs=tg_request_params)
-message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
-file_handler = MessageHandler(Filters.document, handle_file)
 dispatcher = tg_updater.dispatcher
-dispatcher.add_handler(message_handler)
-dispatcher.add_handler(file_handler)
+dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), handle_message))
+dispatcher.add_handler(MessageHandler(Filters.document, handle_file))
+dispatcher.add_handler(CallbackQueryHandler(handle_button_callback))
 dispatcher.add_error_handler(error_callback)
 
 st = ScheduleThread(user_service, tg_updater.bot, deluge_client)
--- user_service.py
+++ user_service.py
@@ -15,6 +15,7 @@ class TorrentStatus(Enum):
 
 class UserService:
     _TORRENT_TABLE = "torrents"
+    _CACHE_TABLE = "cache"
 
     def __init__(self, db_file):
         self.conn = sqlite3.connect(db_file, check_same_thread=False)
@@ -30,14 +31,25 @@ class UserService:
         deluge_torrent_status text NOT NULL ,
         UNIQUE(tg_user_id,deluge_torrent_id) )
         """
-        self.conn.execute(sql_create_auth_token)
 
-    def create_torrent(self, tg_user_id: int, deluge_torrent_id: str):
+        sql_create_cache = f"""CREATE TABLE IF NOT EXISTS {self._CACHE_TABLE} (
+        key text NOT NULL,
+        value text NOT NULL,
+        create_time text NOT NULL,
+        ttl_seconds integer NOT NULL)
+        """
+        sql_cache_index = f"CREATE UNIQUE INDEX IF NOT EXISTS idx_key ON {self._CACHE_TABLE} (key);"
+        self.conn.execute(sql_create_auth_token)
+        self.conn.execute(sql_create_cache)
+        self.conn.execute(sql_cache_index)
+
+    def create_torrent(self, tg_user_id: int, deluge_torrent_id: str, override_on_exist=False):
         x = (datetime.utcnow().isoformat(), datetime.utcnow().isoformat(), tg_user_id, deluge_torrent_id,
              str(TorrentStatus.CREATED))
         with self.conn:
             self.conn.execute(
-                f"INSERT INTO {self._TORRENT_TABLE} (create_time, last_update_time, tg_user_id, deluge_torrent_id, "
+                f"INSERT {'OR REPLACE' if override_on_exist else ''} INTO {self._TORRENT_TABLE} "
+                f"(create_time, last_update_time, tg_user_id, deluge_torrent_id, "
                 f"deluge_torrent_status) VALUES (?,?,?,?,?)",
                 x)
 
@@ -54,6 +66,20 @@ class UserService:
         r = c.execute(f"SELECT tg_user_id, deluge_torrent_id, deluge_torrent_status FROM {self._TORRENT_TABLE} "
                       f"WHERE deluge_torrent_status IS NOT '{TorrentStatus.DOWNLOADED}'")
         return c.fetchall()
+
+    def create_cache(self, key, value, ttl_seconds=60 * 60 * 24 * 30, override_on_exist=False) -> None:
+        x = (key, value, datetime.utcnow().isoformat(), ttl_seconds)
+        with self.conn:
+            self.conn.execute(
+                f"INSERT {'OR REPLACE' if override_on_exist else ''} INTO {self._CACHE_TABLE} "
+                f"(key, value, create_time, ttl_seconds) VALUES (?,?,?,?)",
+                x)
+
+    def get_cache(self, key) -> dict:
+        c = self.conn.cursor()
+        r = c.execute(f"SELECT key, value, create_time, ttl_seconds FROM {self._CACHE_TABLE} "
+                      f"WHERE key = '{key}'")
+        return c.fetchone()
 
     def disconnect(self):
         self.conn.close()
