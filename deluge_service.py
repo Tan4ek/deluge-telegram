9df334a6a1e3bf4350fde929e09fbfe57ccb14ce
Sergei Samokhvalov <kolobok205@gmail.com>
2020-11-11 22:08:42 +0300

    fix docker python version, add deluge label

--- deluge_service.py
+++ deluge_service.py
@@ -1,4 +1,4 @@
-
+from distutils.util import strtobool
 from typing import Dict, List
 
 from deluge_client import DelugeRPCClient
@@ -8,19 +8,35 @@ class DelugeService:
 
     def __init__(self, config):
         self._deluge_client = DelugeRPCClient(config.get('deluge', 'host'),
-                                int(config.get('deluge', 'port')),
-                                config.get('deluge', 'username'),
-                                config.get('deluge', 'password'),
-                                decode_utf8=True)
+                                              int(config.get('deluge', 'port')),
+                                              config.get('deluge', 'username'),
+                                              config.get('deluge', 'password'),
+                                              decode_utf8=True)
         self._deluge_client.connect()
+        self._label_enable = False
+        try:
+            self._label_enable = bool(strtobool(config.get('deluge', 'LabelEnable', fallback='false')))
+        except ValueError:
+            pass
+
+        self._label_id = config.get('deluge', 'LabelId', fallback=None)
+
+        if self._is_label_enabled():
+            self.create_label(self._label_id)
 
     def add_torrent_magnet(self, magnet_url: str) -> str:
         # https://github.com/deluge-torrent/deluge/blob/deluge-2.0.3/deluge/core/core.py#L556
-        return self._deluge_client.core.add_torrent_magnet(magnet_url, {})
+        torrent_id = self._deluge_client.core.add_torrent_magnet(magnet_url, {})
+        if self._is_label_enabled():
+            self.set_torrent_label(torrent_id, self._label_id)
+        return torrent_id
 
     def add_torrent_file(self, file_name: str, file_base64_str: str) -> str:
         # https://github.com/deluge-torrent/deluge/blob/deluge-2.0.3/deluge/core/core.py#L407
-        return self._deluge_client.core.add_torrent_file(file_name, file_base64_str, {})
+        torrent_id = self._deluge_client.core.add_torrent_file(file_name, file_base64_str, {})
+        if self._is_label_enabled():
+            self.set_torrent_label(torrent_id, self._label_id)
+        return torrent_id
 
     def create_label(self, label_id: str):
         # https://github.com/deluge-torrent/deluge/blob/deluge-2.0.3/deluge/plugins/Label/deluge_label/core.py#L178
@@ -57,6 +73,9 @@ class DelugeService:
     def torrent_status(self, torrent_id: str) -> Dict[str, str]:
         # https://github.com/deluge-torrent/deluge/blob/deluge-2.0.3/deluge/core/core.py#L758
         return self._deluge_client.core.get_torrent_status(torrent_id, ['name', 'state'])
+
+    def _is_label_enabled(self):
+        return self._label_enable and self._label_id
 
     def disconnect(self):
         self._deluge_client.disconnect()
